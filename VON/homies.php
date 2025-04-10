1170b7ff7b89102dbbdf6e27d99e6c3f1556b32e
damokloy <pvonjushua@gmail.com>
2025-04-10 18:12:49 +0800

    Add files via upload

---
+++ VON/homies.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ruin your self </h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>age</th>
+
+
+
+            
+            </tr>
+            <thead>
+                <tbody>
+                    <tr>
+                        <td>3 </td>
+                        <td>von </td>
+                        <td> jush</td>
+                        <td>21</td>
+                    </tr>
+                    <tr>
+
+                    <td>3 </td>
+                        <td>von </td>
+                        <td> jush</td>
+                        <td>21</td>
+                    </tr>
+                </tbody>
+    </table>
+</body>
+</html>
